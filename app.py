7366bd14c8324f81fd27693351c71251f77fb3ed
Shumaila-tariq <shumailatariq699@gmail.com>
2026-07-01 12:31:29 +0500

    Enhance loading feedback in Sales Forecasting App
    Added loading messages for model and polynomial features.

--- app.py
+++ app.py
@@ -3,7 +3,10 @@ import joblib
 
 st.title("Sales Forecasting App")
 
+st.write("Loading model...")
 model = joblib.load("polynomial_model.pkl")
-poly = joblib.load("poly_features.pkl")
+st.success("Model loaded successfully!")
 
-st.success("Both files loaded successfully!")
+st.write("Loading Polynomial Features...")
+poly = joblib.load("poly_features.pkl")
+st.success("Polynomial Features loaded successfully!")
